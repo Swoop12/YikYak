e4a3fef49a17fe06e42977153b5e2e3fbe61d1ce
Trevor Adcock <trevor.adcock@mac.com>
2018-09-27 18:13:01 -0600

    Initial Commit

---
+++ YikYak/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YikYak
+//
+//  Created by DevMountain on 9/27/18.
+//  Copyright © 2018 trevorAdcock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
